050ec3db72775f778202f8b4f69d27bc76d7743c
Jace Bennest <jacebennest87@gmail.com>
2019-07-11 23:41:42 -0700

    everything working by running basic.sh

--- basic.sh
+++ basic.sh
@@ -8,7 +8,7 @@ OVMF=$VMDIR/firmware
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m 12G \
     -machine q35,accel=kvm \
     -smp 4,cores=2 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
@@ -20,9 +20,11 @@ qemu-system-x86_64 \
     -device ich9-intel-hda -device hda-output \
     -usb -device usb-kbd -device usb-mouse \
     -netdev user,id=net0 \
-    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -device e1000-82545em,netdev=net0,id=net0,mac=35:ad:66:58:7d:9b \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file=Hackintosh.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
